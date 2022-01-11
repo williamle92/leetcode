dd46b716ca77fe742f98feae2eb1a12b0432a823
williamle92 <william1992le@gmail.com>
2022-01-10 21:20:58 -0800

    practice building out a linked list

---
+++ reverse_linked_list.py
@@ -0,0 +1,50 @@
+"""
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+Example 2:
+
+
+Input: head = [1,2]
+Output: [2,1]
+Example 3:
+
+Input: head = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
+"""
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+node1 = ListNode(1)
+node2 = ListNode(2)
+node3 = ListNode(3)
+node4 = ListNode(4)
+node5 = ListNode(5)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+current_node = node1
+
+while True:
+    print(current_node.val, "->",)
+    if current_node.next is None:
+        print("None")
+        break
+    current_node = current_node.next
