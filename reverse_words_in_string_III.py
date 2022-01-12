7637641eefc9975a1acf9a0a03ccd3ca037e6eeb
williamle92 <william1992le@gmail.com>
2022-01-11 23:32:16 -0800

    completed first solution

--- reverse_words_in_string_III.py
+++ reverse_words_in_string_III.py
@@ -13,3 +13,27 @@ Input: s = "God Ding"
 Output: "doG gniD"
 
 """
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        arr = list(s)
+        left, right = 0,0
+        for index, value in enumerate(arr):
+            if value == " ":
+                right = index -1
+                self.reverser(arr, left, right)
+                left = index + 1
+                
+            if index == len(arr) -1:
+                right = index
+                self.reverser(arr, left, right)
+                
+        return "".join(arr)
+    
+    
+    def reverser(self, word, left, right):
+        while left < right:
+            word[left], word[right] = word[right], word[left]
+            left += 1
+            right -= 1
+        return word
