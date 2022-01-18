326ec4fdfb46ab2b608dc41f0143b66cdf8bfd77
williamle92 <william1992le@gmail.com>
2022-01-17 21:55:05 -0800

    solved the problem using dfs

--- leaf_similar_tree.py
+++ leaf_similar_tree.py
@@ -28,3 +28,48 @@ Constraints:
 The number of nodes in each tree will be in the range [1, 200].
 Both of the given trees will have values in the range [0, 200].
 """
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+    
+    def l(self, val):
+        self.left = TreeNode(val)
+        return self.left
+    def r(self, val):
+        self.right = TreeNode(val)
+        return self.right
+
+
+class Solution:
+    def leafSimilar(self, root1, root2):
+        return self.dfs(root1) == self.dfs(root2)
+#   we are going to depth first search the branch 
+    def dfs(self,root):
+        if root.left is None and root.right is None:
+            return [root.val]
+
+        if root.left and root.right:
+            return self.dfs(root.left) + self.dfs(root.right)
+        
+        if root.right:
+            return self.dfs(root.right)
+        if root.left:
+            return self.dfs(root.left)
+        
+tree = TreeNode(1)
+l_tree =tree.l(2)
+l_tree.l(3)
+r_tree = tree.r(3)
+r_tree.l(2)
+r_tree.r(4)
+
+tree2 = TreeNode(1)
+tree2.l(3)
+r_tree2 = tree2.r(2)
+r_tree2.l(2)
+r_tree2.r(4)
+
+s = Solution()
+print(s.leafSimilar(tree, tree2))
