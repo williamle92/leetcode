9cc8fe2b35b529a00ef827cc4cf3b02eeddcb328
williamle92 <william1992le@gmail.com>
2022-01-18 20:52:10 -0800

    pow leetcode

--- best_time_buy_stock.py
+++ best_time_buy_stock.py
@@ -26,9 +26,11 @@ Constraints:
 0 <= prices[i] <= 104
 
 """
+
+
 class Solution:
     def maxProfit(self, prices):
-        max_profit = min_price = 0
+        max_profit, min_price = 0, float('inf')
         
         for price in prices:
             min_price = min(min_price, price)
