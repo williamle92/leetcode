d5e216c6ea536b7d0557ccf3489681f8920fdedb
williamle92 <william1992le@gmail.com>
2022-01-18 21:42:14 -0800

    completed the problem

--- implement_queue.py
+++ implement_queue.py
@@ -30,3 +30,33 @@ myStack.pop(); // return 2
 myStack.empty(); // return False
 """
 
+from collections import deque
+
+
+class MyStack:
+    def __init__(self):
+        self.arr = deque()
+
+    def push(self, x: int) -> None:
+        self.arr.appendleft(x)
+        
+        
+
+    def pop(self) -> int:
+        e = self.arr.popleft()
+        return e
+    
+    def top(self) -> int:
+        return self.arr[0]
+
+    def empty(self) -> bool:
+        return True if not self.arr else False
+        
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
