fbb71a6d31dfccc9802d6a76adde12ee0b5cc60a
williamle92 <william1992le@gmail.com>
2022-01-18 21:34:06 -0800

    finished stack

---
+++ impplement_stack.py
@@ -0,0 +1,59 @@
+"""
+Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+
+Implement the MyStack class:
+
+void push(int x) Pushes element x to the top of the stack.
+int pop() Removes the element on the top of the stack and returns it.
+int top() Returns the element on the top of the stack.
+boolean empty() Returns true if the stack is empty, false otherwise.
+Notes:
+
+You must use only standard operations of a queue, which means that only push to back, peek/pop from front, size and is empty operations are valid.
+Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue) as long as you use only a queue's standard operations.
+ 
+
+Example 1:
+
+Input
+["MyStack", "push", "push", "top", "pop", "empty"]
+[[], [1], [2], [], [], []]
+Output
+[null, null, null, 2, 2, false]
+
+Explanation
+MyStack myStack = new MyStack();
+myStack.push(1);
+myStack.push(2);
+myStack.top(); // return 2
+myStack.pop(); // return 2
+myStack.empty(); // return False
+ 
+
+Constraints:
+
+1 <= x <= 9
+At most 100 calls will be made to push, pop, top, and empty.
+All the calls to pop and top are valid.
+
+"""
+
+from collections import deque
+
+
+class MyStack:
+    def __init__(self):
+        self.arr = deque()
+
+    def push(self, x: int) -> None:
+        self.arr.appendleft(x)
+        
+    def pop(self) -> int:
+        e = self.arr.pop()
+        return e
+    
+    def top(self) -> int:
+        return self.arr[0]
+
+    def empty(self) -> bool:
+        return True if not self.arr else False
