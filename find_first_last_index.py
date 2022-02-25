149d2ffcfefb5c44c7f3d5ac63f85da3ad5985b9
williamle92 <william1992le@gmail.com>
2022-02-24 19:02:46 -0800

    solved solution with comments

--- find_first_last_index.py
+++ find_first_last_index.py
@@ -30,3 +30,37 @@ nums is a non-decreasing array.
 '''
 
 
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        left = self.binarySearch(nums, target, True)
+        right = self.binarySearch(nums, target, False)
+        
+        return [left, right]
+        
+        
+#    helper function to do the actual binary search
+#   left bias is going to be a boolean
+#   if true, it will point the r pointer to the left of middle and check to see if the new middle is the target, if so then it will be the new index
+#    if false, it will do the opposite but to the right side
+    def binarySearch(self, nums, target, leftBias):
+        left, right = 0, len(nums) -1
+#         we set -1 because the function says to return -1 if it is not found
+        index = -1
+        while left <= right:
+#           middle will set to the middle which is sum of left and right floor division
+            middle = (left + right)// 2
+    
+            if nums[middle] > target:
+                right = middle - 1
+            elif nums[middle] < target:
+                left = middle + 1
+            else:
+#                 if the middle is equal to the target, then we set index = to middle
+                index = middle
+                if leftBias:
+#             we are going to check the left side now to see if it is equal to the target, if so it will run one more time and set the new index equal to the left most
+                    right = middle - 1
+#                 we are going to do the same with the right side now, check to see if there is a right most value that matches the target. (remember r + r//2 m will equal the r pointer, if it is equal to the target then)
+                else:
+                    left = middle + 1
+        return index
